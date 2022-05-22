e3c5f89d3b1c1825f8a2eb76e5053b2d7b285f9b
danlozano77 <danlozano77.gmail.com>
2022-05-22 16:41:55 -0400

    added allNewDice function

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,6 +1,21 @@
 import React from "react"
 import Die from "./components/Die.js"
 export default function App() {
+
+    allNewDice()
+
+    function allNewDice() {
+        const diceArray = [];
+
+        for (let i = 0; i < 10; i++) {
+            const randomNumber = Math.ceil(Math.random() * 6)
+            diceArray[i] = randomNumber
+            console.log(diceArray);
+        }
+        return diceArray
+        
+    }
+
     return (
         <main>
             <div className="dice-container">  
