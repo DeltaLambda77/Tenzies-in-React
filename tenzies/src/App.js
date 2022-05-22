c88a6fc5d6a304083da4b91ea9436135666d624f
danlozano77 <danlozano77.gmail.com>
2022-05-22 16:23:14 -0400

    added components folder

---
+++ tenzies/src/App.js
@@ -0,0 +1,40 @@
+import React from "react"
+import Die from "./components/Die.js"
+export default function App() {
+    return (
+        <main>
+            <div className="dice-container">  
+                <Die 
+                    value={1}
+                />
+                <Die 
+                    value={1}
+                />
+                <Die 
+                    value={1}
+                />
+                <Die 
+                    value={1}
+                />
+                <Die 
+                    value={1}
+                />
+                <Die 
+                    value={1}
+                />
+                <Die 
+                    value={1}
+                />
+                <Die 
+                    value={1}
+                />
+                <Die 
+                    value={1}
+                />
+                <Die 
+                    value={1}
+                />
+            </div>  
+        </main>
+    )
+}
